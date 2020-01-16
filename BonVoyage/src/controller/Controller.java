4c79c4574231340a2823194230bf51d2e4205f84
chrisjusel <christian00.vitale@gmail.com>
2020-01-16 17:14:19 +0100

    controller base

--- BonVoyage/src/controller/Controller.java
+++ BonVoyage/src/controller/Controller.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import forms.LoginFrame;
 import forms.PostFrame;
 import forms.RegisterFrame;
@@ -19,7 +23,20 @@ public class Controller {
 	    ResultsFrame Results = new ResultsFrame(control);
 	    ReviewFrame Review = new ReviewFrame(control);
 	    Search.setVisible(true);
-		Search.setVisible(true);
+	}
+	
+	public Controller() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "root");
+			
+			
+		} catch (SQLException | ClassNotFoundException e){e.printStackTrace();}
+		
 	}
 
 }
