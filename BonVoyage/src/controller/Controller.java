998549cb0cd9a77fd067ac5be383a2c81df6f09e
silve <silve@DESKTOP-8IT8VA1>
2020-01-16 17:05:09 +0100

    Controller update

--- BonVoyage/src/controller/Controller.java
+++ BonVoyage/src/controller/Controller.java
@@ -1,5 +1,10 @@
 package controller;
 
+import forms.LoginFrame;
+import forms.PostFrame;
+import forms.RegisterFrame;
+import forms.ResultsFrame;
+import forms.ReviewFrame;
 import forms.SearchFrame;
 
 public class Controller {
@@ -8,6 +13,12 @@ public class Controller {
 		
 		Controller control = new Controller();
 		SearchFrame Search = new SearchFrame(control);
+		LoginFrame Login = new LoginFrame(control);
+	    RegisterFrame Register = new RegisterFrame(control);
+	    PostFrame Post = new PostFrame(control);
+	    ResultsFrame Results = new ResultsFrame(control);
+	    ReviewFrame Review = new ReviewFrame(control);
+	    Search.setVisible(true);
 		Search.setVisible(true);
 	}
 
