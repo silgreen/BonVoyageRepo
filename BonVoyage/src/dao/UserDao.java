cd8ccd27c09be13fdbc1a37b83b8abd65c3fab08
silve <silve@DESKTOP-8IT8VA1>
2020-01-16 19:51:57 +0100

    funzione di registrazione

--- BonVoyage/src/dao/UserDao.java
+++ BonVoyage/src/dao/UserDao.java
@@ -1,7 +1,37 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import classi.User;
 
 public class UserDao extends User {
-
+	private Connection con;
+	
+	public UserDao(Connection c) {
+		con = c;
+	}
+	
+	public void insertUserInDb(String email, String username, String password, String region, String city) {
+		ResultSet result;
+		String query = "insert into utente(email,username,password,regione,città) values(?,?,?,?,?)";
+		User user;
+		
+		try {
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1,email);
+			pst.setString(2,username);
+			pst.setString(3,password);
+			pst.setString(4,region);
+			pst.setString(5,city);
+			
+			result = pst.executeQuery();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
---
+++ BonVoyage/src/except/PasswordDismatchException.java
@@ -0,0 +1,9 @@
+package except;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class PasswordDismatchException extends Exception {
+	public PasswordDismatchException(){
+	}
+}
